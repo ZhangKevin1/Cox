b7ecf3ea35bdb06db74ecd8828c2e747a024c01b
ZhangKevin1 <977548213@qq.com>
2019-06-05 13:28:28 +0800

    先提交不merge

--- test.py
+++ test.py
@@ -1,6 +1,1 @@
-import pandas
-import statsmodels.formula.api as smf
-
-data = pandas.read_csv('E:\\brain_size.csv', sep=";", na_values=".")
-models = smf.ols("VIQ ~ Gender + 1", data).fit()
-print(models.summary())
+def str2float(s):
